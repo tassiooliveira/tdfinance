b8a439e93ac187d671f1604b5c560f0240f6c5a4
tassiooliveira <tassiooliveira.f@gmail.com>
2022-05-09 21:21:20 -0300

    rota api

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -9,7 +9,7 @@ export function Summary() {
     const { transactions } = useTransactions()
 
   const sumary = transactions.reduce((acc, transaction) => {
-        if ( transaction.type == "deposit") {
+        if ( transaction.type === "deposit") {
             acc.deposits += transaction.amount
             acc.total += transaction.amount
         } else {
